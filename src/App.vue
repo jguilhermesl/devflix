33abbb816fdf5189ac01fdfb571ffd31d0cf1a6f
João Guilherme <joaoguilherme_pe@hotmail.com>
2022-05-26 11:06:26 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Header />
+  <Home />
+</template>
+
+<script>
+import Header from "./components/Header/header.vue";
+import Home from "./components/Home/home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Home,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Mulish", sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+
+button:hover  {
+  opacity: 0.8;
+}
+
+button:active {
+  opacity: 0.6;
+}
+</style>
---
+++ src/components/Header/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <div class="logo">
+            <span><i class="fa-solid fa-video"></i> DEVFLIX</span>
+        </div>
+        <ul>
+            <li>Início</li>
+            <li>Filmes</li>
+            <li>Séries</li>
+            <li>Animes</li>
+            <li>Aplicativo</li>
+        </ul>
+        <div class="actions">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <span><i class="fa-solid fa-user"></i> Minha conta</span>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+
+</style>
